e8f9230d168903394223c201a9b280ac09a53fe6
Orestes Papanastassiou <orestpap@gmail.com>
2019-04-02 00:59:35 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,7 +1,5 @@
-from open_write_files import list_lines_txt
+from open_write_files import list_lines_txt,write_to_text_file
 import re
-#from itertools import zip_longest
-
 
 def objective_function_check(firstline):
 	minmaxmap={'min':-1,'max':1}
@@ -13,24 +11,24 @@ def objective_function_check(firstline):
 		return c_table,minmaxmap[bits[0]]		
 
 	else:
-		raise ValueError('Missing min or max tag before objective function in line 1')
+		raise TypeError('Missing min or max tag before objective function in line 1')
 		
 
 def check_plus_minus(bits):
 	if bits[0] in ('-','+'):
 		for abit in bits[0::2]:
 			if abit not in ('+','-'):
-				raise ValueError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
+				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
 		return 1 #check even
 	else:
 
 		for abit in bits[1::2]:
 			if abit not in ('-','+'):
-				raise ValueError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
+				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
 		return 0 #check odd
 
 def check_xs(bits,flag,key,var_count=None):
-	wheretostartfrom,isobjective,prev={1:1,0:0},{'o':1,'c':0},0
+	wheretostartfrom,prev={1:1,0:0},0
 	
 	start=wheretostartfrom[flag]
 	
@@ -40,20 +38,20 @@ def check_xs(bits,flag,key,var_count=None):
 		check=pattern.search(abit)
 		
 		if not check:
-			raise ValueError(f'Objective function variables must be of format: INTEGERXi\ni.e. 5x1, 2.5x1\nYours were: {abit}')
+			raise TypeError(f'Objective function variables must be of format: INTEGERXi\ni.e. 5x1, 2.5x1\nYours were: {abit}')
 		
 		
 		xi=abit.split('x')[-1]
 		
-		if isobjective[key]:
+		if key=='o':
 			if int(xi)!=prev+1:
-				raise ValueError(f'Objective function variable identifiers (i) must follow a specific order: i= (1,2,3,...n)\nCorrect input: x{prev+1}\nYour input: x{xi}')
+				raise TypeError(f'Objective function variable identifiers (i) must follow a specific order: i= (1,2,3,...n)\nCorrect input: x{prev+1}\nYour input: x{xi}')
 			
 			prev+=1
 		else:
 			if int(xi)<=prev or int(xi)>var_count:
-				raise ValueError(f'Constrain\'s variable identifiers (i) must follow a specific order: i= (1,2,3,...n) and belong to the range of 1 to {var_count}\nCorrect input: x{prev+1}\nYour input: x{xi}')
-		
+				raise TypeError(f'Constrain\'s variable identifiers (i) must follow a specific order: i= (1,2,3,...n) and belong to the range of 1 to {var_count}\nCorrect input: x{prev+1}\nYour input: x{xi}')
+			prev=int(xi)
 
 	return table(bits,flag,key,var_count)
 
@@ -71,15 +69,15 @@ def table(bits,flag,key,var_count=None):
 	weights,operands=[abit.split('x')[0] if abit.split('x')[0]!='' else '1' for abit in bits[xs_pos::2]],[abit for abit in bits[operands_pos::2]]
 			
 	if len(weights)!=len(operands):
-		operands.insert(0,'+') #if lists' sizes not equal, it means that the operand for x1's weight is +
+		operands.insert(0,'+') #if lists' sizes not equal, it means that the first variable's weight is + and the user did not provide it
 
 	table=list(float(atuple[0]+atuple[1]) for atuple in zip(operands,weights))
 	
 	if key=='c':
 		#add 0 for vars with weight 0
 		
-		varsin=[int(abit.split('x')[-1]) for abit in bits[xs_pos::2]]
-		varsmissing=[i for i in range(1,var_count+1) if i not in varsin]
+		varsin=[int(abit.split('x')[-1]) for abit in bits[xs_pos::2]] #variables without 0 weight in constrains inequalities
+		varsmissing=[i for i in range(1,var_count+1) if i not in varsin] #variables with 0 weight
 		for i in varsmissing:
 			table.insert(i-1,0)
 			
@@ -95,11 +93,11 @@ def constrains_check(lines,var_count):
 	
 	for index,aline in enumerate(lines):
 
-		bits=list(map(lambda abit: abit.lower(),filter(None,aline.replace('\t','').split(' ')))) #removes potential \t characters and splits text line on spaces
+		bits=list(map(lambda abit: abit.lower(),filter(None,aline.replace('\t','').split(' ')))) #removes potential \t characters, splits text line on spaces and lowercases all chars
 		
 		if index==0:
 			st_check(bits[0])
-			bits.pop(0)
+			bits.pop(0) #removes 'subject to' tag
 		
 		eqin.append(ineq_operand_check(bits[-2]))
 		b_table.append(get_right_side_of_ineq(bits[-1]))
@@ -114,32 +112,29 @@ def ineq_operand_check(abit):
 	if eqdict.get(abit) or abit=='=': #for some reason get(key) doesn't recognize = as a key of the dictionary
 		return eqdict[abit]
 	
-	raise ValueError(f'Second to last character in constrains must be one of the following: >, >=, <, <=, =\nYour input was: {abit}')
+	raise TypeError(f'Second to last character in constrains must be one of the following: >, >=, <, <=, =\nYour input was: {abit}')
 
 def get_right_side_of_ineq(abit):
 	if re.match(r'^\d+(\.\d+)?$', abit):
 		return float(abit)
 
-	raise ValueError(f'Right side of inequality must contain numbers.\ni.e. 2, 2.5\nYour input was {abit}')
+	raise ValueError(f'Right side of inequality must contain numbers and be great than zero.\ni.e. 2, 2.5\nYour input was {abit}')
 
 
 def st_check(abit):
 	if abit not in ('s.t.', 'st', 'subject'):
-		raise ValueError(f'Subject to tag missing or misspelled in first constrain.\nAvailable options: s.t., st, subject\nYour input was {abit}')
+		raise TypeError(f'Subject to tag missing or misspelled in first constrain.\nAvailable options: s.t., st, subject\nYour input was {abit}')
 			
 	
 def remove_empty_lines(lp1):
 	
 	return list(filter(lambda x: x!='',filter(lambda x: x!='\t',filter(lambda x: not re.match(r'^\s*$', x), lp1)))) 
-	#removes empty lines, line containining \t character, and lines containing just space characters
+	#removes empty lines, lines containining \t character, and lines containing just whitespace characters
 
 
 lp1=remove_empty_lines(list_lines_txt('lp1.txt'))
 
 c,MinMax=objective_function_check(lp1[0])
-
-print(c)
 a,b,eqin=constrains_check(lp1[1:len(lp1)-1],len(c))
-print(a)
-print(b)
-print(eqin)
+
+write_to_text_file('outputfile.txt',f"Min or Max:\n{MinMax}\nC Table:\n{c}\nA table:\n{a}\nInequalities operands (eqin table):\n{eqin}\nB table:\n{b}")
