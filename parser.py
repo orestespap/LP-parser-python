9b4dd14cb3bf3ce13ef5957faa118dcd4e54cefe
Orestes Papanastassiou <orestpap@gmail.com>
2019-04-03 18:54:44 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from open_write_files import list_lines_txt,write_to_text_file
+from file_manager import list_lines_txt,write_to_text_file
 import re
 
 def objective_function_check(firstline):
@@ -18,13 +18,13 @@ def check_plus_minus(bits):
 	if bits[0] in ('-','+'):
 		for abit in bits[0::2]:
 			if abit not in ('+','-'):
-				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
-		return 1 #check even
+				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operator and operand.\nAt least one space between operand and operator is required.\ni.e. x1 + 2x2')
+		return 1 #operands must be in even array positions
 	else:
 		for abit in bits[1::2]:
 			if abit not in ('-','+'):
-				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operands and variable')
-		return 0 #check odd
+				raise TypeError('Missing +/- symbol or you didn\'t leave a space between operator and operand.\nAt least one space between operand and operator is required.\ni.e. x1 + 2x2')
+		return 0 #operands must be in odd array positions
 
 def check_xs(bits,flag,key,var_count=None):
 	wheretostartfrom,prev={1:1,0:0},0
@@ -37,18 +37,18 @@ def check_xs(bits,flag,key,var_count=None):
 		check=pattern.search(abit)
 		
 		if not check:
-			raise TypeError(f'Objective function variables must be of format: INTEGERXi\ni.e. 5x1, 2.5x1\nYours were: {abit}')
+			raise TypeError(f'Objective function variables must be of format: INTEGERXi\ni.e. 5x1, 2.5x1\nYour input was: {abit}')
 
 		xi=abit.split('x')[-1]
 		
 		if key=='o':
 			if int(xi)!=prev+1:
-				raise TypeError(f'Objective function variable identifiers (i) must follow a specific order: i= (1,2,3,...n)\nCorrect input: x{prev+1}\nYour input: x{xi}')
+				raise TypeError(f'Objective function variable identifiers (i) must follow a specific order: i= (1,2,3,...n)\nYour input was: x{xi}')
 			
 			prev+=1
 		else:
 			if int(xi)<=prev or int(xi)>var_count:
-				raise TypeError(f'Constrain\'s variable identifiers (i) must follow a specific order: i= (1,2,3,...n) and belong to the range of 1 to {var_count}\nCorrect input: x{prev+1}\nYour input: x{xi}')
+				raise TypeError(f'Constrain\'s variable identifiers (i) must follow a specific order: i= (1,2,3,...n) and belong to the range of 1 to {var_count}\nYour input was: x{xi}')
 			prev=int(xi)
 
 	return table(bits,flag,key,var_count)
@@ -59,16 +59,16 @@ def table(bits,flag,key,var_count=None):
 	xs_pos=wheretostartfrom[flag]
 	
 	if flag:
-		operands_pos=wheretostartfrom[0]
+		operators_pos=wheretostartfrom[0]
 	else:
-		operands_pos=wheretostartfrom[1]
+		operators_pos=wheretostartfrom[1]
 	
-	weights,operands=[abit.split('x')[0] if abit.split('x')[0]!='' else '1' for abit in bits[xs_pos::2]],[abit for abit in bits[operands_pos::2]]
+	weights,operators=[abit.split('x')[0] if abit.split('x')[0]!='' else '1' for abit in bits[xs_pos::2]],[abit for abit in bits[operators_pos::2]]
 			
-	if len(weights)!=len(operands):
-		operands.insert(0,'+') #if lists' sizes not equal, it means that the first variable's operand is + and the user did not type it
+	if len(weights)!=len(operators):
+		operators.insert(0,'+') #if lists' sizes not equal, it means that the first variable's operator is + and the user did not type it
 
-	table=list(float(atuple[0]+atuple[1]) for atuple in zip(operands,weights))
+	table=list(float(atuple[0]+atuple[1]) for atuple in zip(operators,weights))
 	
 	if key=='c':
 		#add 0 for vars with weight 0
@@ -92,31 +92,31 @@ def constrains_check(lines,var_count):
 			st_check(bits[0])
 			bits.pop(0) #removes 'subject to' tag
 		
-		eqin.append(ineq_operand_check(bits[-2]))
+		eqin.append(ineq_operators_check(bits[-2]))
 		b_table.append(get_right_side_of_ineq(bits[-1]))
 		a_table.append(check_xs(bits[:-2],check_plus_minus(bits[:-2]),'c',var_count))
 
 	return a_table,b_table,eqin
 	
 
-def ineq_operand_check(abit):
+def ineq_operators_check(abit):
 	eqdict={'<=':-1,'>=':1,'=':0}
 
 	if eqdict.get(abit) or abit=='=': #for some reason get(key) doesn't recognize = as a key of the dictionary
 		return eqdict[abit]
 	
-	raise TypeError(f'Second to last character in constrains must be one of the following: >, >=, <, <=, =\nYour input was: {abit}')
+	raise TypeError(f'Second to last character in constrains must be one of the following: >=, <=, =\nYour input was: {abit}')
 
 def get_right_side_of_ineq(abit):
 	if re.match(r'^\d+(\.\d+)?$', abit):
 		return float(abit)
 
-	raise ValueError(f'Right side of inequality must contain numbers and be great than zero.\ni.e. 2, 2.5\nYour input was {abit}')
+	raise ValueError(f'Right side of inequality must be a number.\ni.e. 2, 2.5\nYour input was: {abit}')
 
 
 def st_check(abit):
 	if abit not in ('s.t.', 'st', 'subject'):
-		raise TypeError(f'Subject to tag missing or misspelled in first constrain.\nAvailable options: s.t., st, subject\nYour input was {abit}')
+		raise TypeError(f'Subject to tag missing or misspelled in first constrain.\nAvailable options: s.t., st, subject\nYour input was: {abit}')
 			
 	
 def remove_junk_chars(lp1):
@@ -126,4 +126,4 @@ def remove_junk_chars(lp1):
 lp1=remove_junk_chars(list_lines_txt('lp1.txt'))
 c,MinMax=objective_function_check(lp1[0])
 a,b,eqin=constrains_check(lp1[1:len(lp1)-1],len(c))
-write_to_text_file('outputfile.txt',f"Min or Max:\n{MinMax}\nC Table:\n{c}\nA table:\n{a}\nInequalities operands (eqin table):\n{eqin}\nB table:\n{b}")
+write_to_text_file('outputfile.txt',f"Min or Max:\n{MinMax}\n\nC Table:\n{c}\n\nA table:\n{a}\n\nInequalities' operators (eqin table):\n{eqin}\n\nB table:\n{b}")
---
+++ reademe.txt
@@ -0,0 +1,25 @@
+﻿Initially, the parser breaks down the input file into two chunks; the first line, which is supposed to contain the objective function,
+and the rest of the lines minus the last one, which contain the technological and natural constrains respectively.
+
+All the lines (m count) are now stored into a 1xm array. Each line (item in list) then undergoes a whitespace character cleaning process,
+in which whitespace and \t characters are being removed.
+
+To ensure the validity of the objective function's format, the parser splits the text line on spaces, and does the following:
+	- Checks that the first element is either min or max
+	
+	- Then checks the second element of the array. If it is an operand (+ or -), it assumes that all operators are located in the
+	odd positions of the array, while the operands are located in the even positions of the array (and vice versa).
+	
+	- Then it checks if the operators are located in the right positions, and then moves on to checking the operands' positions.
+	When it comes to the latter, along with position checking, the parser also validates their format by using regular expressions.
+	
+	- Once the above have been validated, it moves on to the creation of the C table. It then creates two sub-tables, one contains the
+	operands and the other contains the operators. 
+
+	-It then checks the size of the two arrays; if it isn't even, it assumes that x1's operator is + (the user didn't type it), 
+	zips the two arrays and converts the string items into floats.
+
+The parsing process for the constrains is almost identical. The only noteworthy difference is the addition of 0 values in the operands
+table for the variables that have a 0 weight in a given constrain.
+
+For any further questions or suggestions don't hesitate to email me at orestespap@gmail.com.
